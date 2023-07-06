cb3984d93956c7786b7251db25c35850d2a8d19b
Hassan Mohamed <hassan.mhmd656@gmail.com>
2023-07-06 06:39:04 +0300

    adding app to nsq docker-compose network

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
-const express = require('express')
-const nsq = require('nsqjs')
-const app = express()
-const port = 3000
+const express = require("express");
+const nsq = require("nsqjs");
+const app = express();
+const port = 3000;
 
-const reader = new nsq.Reader('test', 'test', {
-  lookupdHTTPAddresses: 'localhost:32768'
-})
+const reader = new nsq.Reader("test", "test", {
+  lookupdHTTPAddresses: "nsqlookupd:4161",
+});
 
-reader.connect()
+reader.connect();
 
-reader.on('message', msg => {
-  console.log('Received message [%s]: %s', msg.id, msg.body.toString())
-  msg.finish()
-})
+reader.on("message", (msg) => {
+  console.log("Received message [%s]: %s", msg.id, msg.body.toString());
+  msg.finish();
+});
 
-reader.on('error', err => {
-    console.log({err});
-  })
+reader.on("nsqd_connected", () => {
+  console.log("nsqd_connected");
+});
 
-app.listen(port, () => console.log(`NSQ Consumer is listening on port ${port}!`))
+reader.on("ready", () => {
+  console.log("reader ready");
+});
+reader.on("error", (err) => {
+  console.log({ err });
+});
+
+app.listen(port, () =>
+  console.log(`NSQ Consumer is listening on port ${port}!`)
+);
